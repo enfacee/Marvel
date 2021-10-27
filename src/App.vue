c5ab05dad253f4993bc9ddeaf2a39400925a4e58
Pavel <Pavel@vl2003.ar14-24.ekb.ru.mirasystem.net>
2021-10-27 22:02:15 +0500

    “init”

---
+++ src/App.vue
@@ -0,0 +1,83 @@
+<template>
+    <div id="app">
+
+        <app-header/>
+
+        <div class="container">
+            <h1 class="pt-3 pb-3">Персонажи Marvel</h1>
+
+
+            <app-modal/>
+
+            <spinner/>
+
+            <div class="row">
+
+
+            <div 
+                v-for="el in characters"
+                class="card mb-3 col-sm-12 col-md-6 col-lg-4">
+            <div class="row g-0">
+                <div class="col-4">
+                    <img :src="el.thumbnail"
+                         :alt="el.name" 
+                          style="max-width:100%;" 
+                    >
+                </div>
+                <div class="col-8">
+                    <div class="card-body">
+                    <h5 class="card-title">{{el.name}}</h5>
+                        <!-- Button trigger modal -->
+                        <button type="button"  
+                                data-bs-toggle="modal" 
+                                data-bs-target="#exampleModal"
+                                class="btn btn-secondary btn-sm"
+                        >
+                            Подробнее
+                        </button>
+                    </div>
+                </div>
+            </div>
+            </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    import Spinner from "./components/Spinner";
+    import AppModal from "./components/AppModal";
+    import AppHeader from "./components/AppHeader";
+
+    export default {
+        name: 'App',
+        components: {
+            AppHeader,
+            AppModal,
+            Spinner,
+        },
+        data() {
+            return {
+                loading: false,
+                characters: [],
+                characterIndex: 0,
+            }
+        },
+        methods: {
+            fetchCharacters: function(){
+                return fetch('https://netology-api-marvel.herokuapp.com/characters')
+                    .then(res => res.json())
+                    .then(json => this.characters = json);
+            },
+        },
+        computed: {},
+        mounted(){
+            this.fetchCharacters();
+        },
+    }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/AppModal.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Человек-паук</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <img src="http://i.annihil.us/u/prod/marvel/i/mg/3/50/526548a343e4b.jpg"
+                        alt="Человек-паук" style="max-width: 100%;">
+
+                    <div>
+                        <p class="text-muted">2020-07-21T10:30:10-0400</p>
+                        <h5>Описание:</h5>
+                        <p>
+                            Укушенный радиоактивным пауком,
+                            старшеклассник Питер Паркер обрел 
+                            скорость, силу и силу паука. Приняв имя 
+                            Человек-паук, Питер надеялся начать карьеру, 
+                            используя свои новые способности. Наученный тому, 
+                            что с большой силой приходит большая ответственность, 
+                            Паук поклялся использовать свои силы, чтобы помогать людям.
+                        </p>
+
+                        <h5>Комиксы:</h5>
+
+                        <ul>
+                            <li>Adventures of Spider-Man (1996) #1</li>
+                            <li>Adventures of Spider-Man (1996) #2</li>
+                            <li>Adventures of Spider-Man (1996) #3</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Закрыть</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "AppModal",
+    }
+</script>
+
+<style scoped>
+
+</style>
